937881b87380286ba80c756ed503d2b765e845d4
Roland Ndjamba <Roland@student.aoa>
2026-02-11 20:29:37 +0200

    Finished food

---
+++ food.js
@@ -0,0 +1,30 @@
+const images = [
+    '20250711_130253.jpg', '20250711_130919.jpg', '20250711_165940.jpg', 
+    '20250711_190335.jpg', '20250711_191247.jpg', '20250711_191309.jpg', 
+    '20250712_085934.jpg', '20250712_121839.jpg', '20250712_122110.jpg', 
+    '20250712_122114.jpg', '20250712_125106.jpg', '20250712_184959.jpg', 
+    '20250712_185356.jpg', '20250713_080752.jpg', '20250716_204038.jpg', 
+    '20250721_210954.jpg', '20250724_202829.jpg', '20250724_202834.jpg', 
+    '20250724_203529.jpg', '20250726_114321.jpg', '20250726_114324.jpg', 
+    '20250726_114330.jpg', '20250726_114336.jpg', '20250726_114343.jpg', 
+    '20250726_114400.jpg', '20250726_114413.jpg', '20250726_114434.jpg', 
+    '20250726_114451.jpg', '20250726_114503.jpg', '20250726_114518.jpg',
+    '20250726_114532.jpg', '20250726_114546.jpg'
+];
+
+const folder = 'food/';
+const gallery = document.getElementById('image-gallery');
+
+images.forEach((fileName, index) => {
+    const img = document.createElement('img');
+    img.src = folder + fileName;
+    
+    img.style.top = Math.random() * 80 + 5 + '%'; 
+    img.style.animationDelay = (index * 3.5) + 's';
+
+    img.onclick = () => {
+        window.open(img.src, '_blank');
+    };
+    
+    gallery.appendChild(img);
+});
