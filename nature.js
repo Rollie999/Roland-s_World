810d42d701bd0a7cc05e5610dd670561c4d8be90
Roland Ndjamba <Roland@student.aoa>
2026-02-16 17:28:23 +0200

    More pishkors

--- nature.js
+++ nature.js
@@ -1,16 +1,19 @@
-const images = [
-    "IMG-20240814-WA0038.jpg",
-"IMG-20240814-WA0126.jpg",
-"IMG-20240815-WA0019.jpg",
-"IMG-20240815-WA0023.jpg",
-"IMG-20240815-WA0044.jpg",
-"IMG-20240818-WA0013.jpg",
-"Screenshot 2024-12-24 140147.png",
-"Screenshot 2024-12-31 234042.png",
- 
+const images = [ "20250801_185713.jpg",
+"20250815_181617.jpg",
+"20250816_080124.jpg",
+"20250905_120429.jpg",
+"IMG-20240323-WA0009.jpg",
+"20240616_175837.heif",
+"20241030_124512.heif",
+"20250717_064112.jpg",
+"20250717_064446.jpg",
+"20250717_064544.jpg",
+"20250718_075634.jpg",
+"20250721_082302.jpg",
+"20250723_174236.jpg"
 ];
 
-const folder = 'loved/';
+const folder = 'nature/';
 const gallery = document.getElementById('image-gallery');
 
 images.forEach((fileName, index) => {
